ad049199a59272067ef77b56e4a82f0aeef73a63
vanno <vannok@live.com>
2024-01-08 18:48:24 +0300

    name changed

--- index.ts
+++
@@ -1,8 +0,0 @@
-import round from "lodash/round";
-import toNumber from "lodash/toNumber";
-
-const num: number | null | undefined | string = null;
-
-const lodashNum = num ? round(toNumber(num), 2) : undefined;
-
-console.log(lodashNum);
